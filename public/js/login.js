8f30c7c4832eecbcb0712e8352c8d91caa2041a1
lapshaffer <lapshaffer@gmail.com>
2021-09-12 20:34:16 -0400

    logging in and out, signing up added

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#usernameLogin').value.trim();
+    const password = document.querySelector('#passwordLogin').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('#loginForm').addEventListener('submit', loginFormHandler);
---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#usernameSignup').value.trim();
+    const password = document.querySelector('#passwordSignup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            console.log('success');
+
+            document.location.replace('/dashboard');
+
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('#signupForm').addEventListener('submit', signupFormHandler);
